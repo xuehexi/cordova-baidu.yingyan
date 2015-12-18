c313b476a4991b326732dd110b6a2cc23da29d73
wlq <a17618@qq.com>
2015-12-18 17:03:30 +0800

    test

--- src/android/BaiduTrace.java
+++ src/android/BaiduTrace.java
@@ -1,10 +1,29 @@
 package com.bingofresh.plugins.baidutrace;
 
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import android.content.Context;
+import android.util.Log;
+import android.util.SparseArray;
 
-public class BaiduTrace  {
+public class BaiduTrace extends CordovaPlugin {
 
-	public String startTrace(String test) {
-    	return "abc";
-  	}
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        Activity activity = this.cordova.getActivity();
+        if (action.equals("startTrace")) {
+            Intent i = activity.getIntent();
+            if (i.hasExtra(Intent.EXTRA_TEXT)) {
+                callbackContext.success(i.getStringExtra(Intent.EXTRA_TEXT));
+            } else {
+                callbackContext.error("");
+            }
+            return true;
+        }
+        return false;
+    }
 }
--- www/Trace.js
+++ www/Trace.js
@@ -1,5 +1,5 @@
 // baidu yingyan
-var cordova = require('cordova');
+var exec = require('cordova/exec');
 var SERVICE_NAME = 'BaiduTrace';
 var ACTION_START_TRACE = 'startTrace';
 
@@ -8,14 +8,6 @@ var baiduyingyan = module.exports = {};
 
 var idGenerator = 0;
 
-baiduyingyan.startTrace = function (success, error, options) {
-  if (typeof sucess === 'object') {
-    options = success;
-  }
-  if (typeof error === 'object') {
-    options = error;
-  }
-  cordova.exec(function(args) {
-    success.apply(null, args);
-  }, error, SERVICE_NAME, ACTION_START_TRACE, [options]);
+baiduyingyan.getExtra = function(success, error) {
+    exec(success, error, SERVICE_NAME, ACTION_START_TRACE, []);
 };
